9b5562c06688596b17254393298a896fe8fe7620
TelmanAsatryan <telman.asatryan99@gmail.com>
2020-04-10 20:17:21 +0400

    Create BankManager Program

---
+++ HomeWork2/Lesson3/BankManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class BankManager
+    {
+        private int sum;
+        public BankManager(int sum)
+        {
+            this.sum = sum;
+        }
+
+        private void MonyChekZero()
+        {
+            
+                Console.WriteLine("на вашем шето -1000$");
+        }
+
+        private void MonyChak()
+        {
+            int chak;
+
+            Console.WriteLine($"ваш шет попалнилос {sum} ");
+            chak = 1000 - sum;
+            Console.WriteLine($"тепер у вас долг {chak}");
+
+        }
+        private void ChakOllMony()
+        {
+            Console.WriteLine("ваш долг погасен ");
+            Console.WriteLine("на вашем шету 0$");
+        }
+
+        private void ChakMuchMony()
+        {
+            int chak;
+            chak = sum - 1000;
+            Console.WriteLine($"на вашем шету {chak}$");
+        }
+
+        public void PrintBankManager()
+        {
+            if(sum==0)
+                MonyChekZero();
+            if(sum<1000)
+                MonyChak();
+            if(sum==1000)
+                ChakOllMony();
+            if(sum>1000)
+                ChakMuchMony();
+        }
+    }
+
+
+
+}
